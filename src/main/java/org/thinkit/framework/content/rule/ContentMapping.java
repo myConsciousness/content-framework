dd8e16da53950e09ad7c90888dac64963d531ecb
Kato Shinya <yourdream28@gmail.com>
2020-08-14 09:26:58 +0900

    add

---
+++ src/main/java/org/thinkit/framework/content/rule/ContentMapping.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2020 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.thinkit.framework.content.rule;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 使用するコンテンツファイルへのマッピングを行うアノテーションです。
+ *
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ContentMapping {
+
+    /**
+     * {@link Content} インターフェースを実装したEnumクラスを設定します。
+     * <p>
+     * 設定するEnum要素は使用するコンテンツファイルまでの相対パスを持っている必要があります。
+     *
+     * @return コンテンツ
+     */
+    Class<? extends Enum<? extends Content>> content();
+}
